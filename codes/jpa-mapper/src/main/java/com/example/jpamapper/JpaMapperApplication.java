9e57587f237a7dfc2a48a45fad76406c6324b401
UncleSubak <kko6369@naver.com>
2024-03-05 22:33:42 +0900

    2024-03-05 오늘 공부한 내용

---
+++ codes/jpa-mapper/src/main/java/com/example/jpamapper/JpaMapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMapperApplication.class, args);
+    }
+
+}
---
+++ codes/jpa-mapper/src/test/java/com/example/jpamapper/JpaMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
