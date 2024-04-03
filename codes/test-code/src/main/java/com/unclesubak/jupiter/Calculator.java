5cf0b8fb0b4df91a2e8fb9da4f7c3a8bd5ec37f8
UncleSubak <kko6369@naver.com>
2024-04-03 18:13:41 +0900

    24-04-03 불필요 파일 삭제

--- codes/test-code/src/main/java/com/unclesubak/jupiter/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.unclesubak.jupiter;
-
-public class Calculator {
-
-    public int plusTwoNumber (int firstNum, int secondNum) {
-
-        return firstNum + secondNum;
-    }
-}
--- codes/test-code/src/main/java/com/unclesubak/parameterized/NumberValidator.java
+++
@@ -1,8 +0,0 @@
-package com.unclesubak.parameterized;
-
-public class NumberValidator {
-    public static boolean isOdd(int number) {
-
-        return number % 2 != 0;
-    }
-}
