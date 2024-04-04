f2d4e7e78cec4743837b1ecc39005e240c1b989a
UncleSubak <kko6369@naver.com>
2024-04-04 13:49:42 +0900

    24-04-04 Redis IntegrationTest 작성

--- codes/test/dayonetest/src/test/java/com/jyujyu/dayonetest/IntegrationTest.java
+++ codes/test/dayonetest/src/test/java/com/jyujyu/dayonetest/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jyujyu.dayonetest;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.Ignore;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -22,6 +23,7 @@ import java.util.Map;
 public class IntegrationTest {
 
     static DockerComposeContainer rdbms;
+    static RedisContainer redis;
 
     static  {
         rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml"))
@@ -39,6 +41,9 @@ public class IntegrationTest {
                 );
 
         rdbms.start();
+
+        redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6"));
+        redis.start();
     }
 
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -47,10 +52,18 @@ public class IntegrationTest {
         public void initialize(ConfigurableApplicationContext applicationContext) {
             Map<String, String> properties = new HashMap<>();
 
+            // rdbms initialize
             var rdbmsHost = rdbms.getServiceHost("local-db", 3306);
             var rdbmsPort = rdbms.getServicePort("local-db", 3306);
 
             properties.put("spring.datasource.url", "jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort + "/score");
+
+            // redis initialize
+            var redisHost = redis.getHost();
+            var redisPort = redis.getFirstMappedPort();
+
+            properties.put("spring.data.redis.host", redisHost);
+            properties.put("spring.data.redis.port", redisPort.toString());
 
             TestPropertyValues.of(properties)
                     .applyTo(applicationContext);
