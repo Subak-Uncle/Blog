490d46c1517f7ede23f12aa261de0f82c148003b
UncleSubak <kko6369@naver.com>
2024-08-19 19:54:31 +0900

    Move : XR-API 파일 위치 변경

--- codes/xr-api/src/main/java/kr/helloworldlabs/xrapi/response/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-package kr.helloworldlabs.xrapi.response;
-
-public record ApiResponse (
-        String message
-){
-    public static ApiResponse of(String message) {
-        return new ApiResponse(message);
-    }
-}
