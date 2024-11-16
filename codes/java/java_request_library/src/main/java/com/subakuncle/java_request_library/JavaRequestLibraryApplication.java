faa7af7c6c2154692a481528e3c9fd583f7c2f69
UncleSubak <kko6369@naver.com>
2024-11-16 23:31:53 +0900

    2024-11-16 자료 이전

---
+++ codes/java/java_request_library/src/main/java/com/subakuncle/java_request_library/JavaRequestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.subakuncle.java_request_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRequestLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRequestLibraryApplication.class, args);
+    }
+
+}
---
+++ codes/java/java_request_library/src/test/java/com/subakuncle/java_request_library/JavaRequestLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subakuncle.java_request_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRequestLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
